99cfdee8471dee0a7ac7aecb2a6bcf75e39bee1c
virus <virus09@mail.ru>
2018-09-13 23:26:42 +0300

    task love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var loveTrianglesCount = 0;
+    for (var n = 0; n < preferences.length; n++) {
+        var a = n + 1;
+        var b = preferences[a - 1];
+        var c = preferences[b - 1];
+        var d = preferences[c - 1];
+        if (b != c && c != d && b != d) {
+            if (a === d) {
+                loveTrianglesCount++;
+            }
+        }
+    }
+    return loveTrianglesCount / 3;
 };
